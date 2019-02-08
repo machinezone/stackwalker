3e35da40fc532b102469f87effb0199cb491a2b3
Benjamin Sergeant <bsergeant@mz.com>
2019-02-08 14:19:06 -0800

    py2 and py3 modules looks more alike now

--- stackwalker_ext_v2.cc
+++ stackwalker_ext_v2.cc
@@ -1,11 +1,19 @@
 
+#include <stdio.h>
 #include <Python.h>
 
-#include <string>
+extern PyObject *
+stackwalker_stackwalk(PyObject *self, PyObject *args);
 
-extern PyObject * stackwalker_stackwalk(PyObject *self, PyObject *args);
 
-static PyMethodDef StackwalkerMethods[] = {
+// Method definition object for this extension, these argumens mean:
+// ml_name: The name of the method
+// ml_meth: Function pointer to the method implementation
+// ml_flags: Flags indicating special features of this method, such as
+//          accepting arguments, accepting keyword arguments, being a
+//          class method, or being a static method of a class.
+// ml_doc:  Contents of this method's docstring
+static PyMethodDef stackwalker_methods[] = { 
     {"stackwalk",  stackwalker_stackwalk, METH_VARARGS,
      "Execute a shell command."},
     {NULL, NULL, 0, NULL}        /* Sentinel */
@@ -14,5 +22,5 @@ static PyMethodDef StackwalkerMethods[] = {
 PyMODINIT_FUNC
 initstackwalker(void)
 {
-    (void) Py_InitModule("stackwalker", StackwalkerMethods);
+    (void) Py_InitModule("stackwalker", stackwalker_methods);
 }
